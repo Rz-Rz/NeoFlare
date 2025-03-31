56158c53f0fe270b82f4cac567f7d5f8831a0398
Karim Dhrif <karimdhrif4444@gmail.com>
2025-03-31 16:33:25 +0200

    updated nvim v2

--- nvim_v2/lua/configs/lspconfig.lua
+++ nvim_v2/lua/configs/lspconfig.lua
@@ -1,20 +1,29 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 
 -- EXAMPLE
-local servers = { "html", "cssls", "clangd" }
-local nvlsp = require "nvchad.configs.lspconfig"
+local servers = { "html", "cssls", "jedi_language_server", "basedpyright" }
+local nvlsp = require("nvchad.configs.lspconfig")
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
+	lspconfig[lsp].setup({
+		on_attach = nvlsp.on_attach,
+		on_init = nvlsp.on_init,
+		capabilities = nvlsp.capabilities,
+	})
 end
+
+-- Configure clangd explicitly with clang-tidy enabled
+lspconfig.clangd.setup({
+	on_attach = nvlsp.on_attach,
+	on_init = nvlsp.on_init,
+	capabilities = nvlsp.capabilities,
+	-- Add the --clang-tidy flag to the command used to launch clangd.
+	cmd = { "clangd", "--clang-tidy", "--clang-tidy-checks=-cppcoreguidelines-pro-type-vararg" },
+})
 
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
